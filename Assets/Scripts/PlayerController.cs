f1e6b9db1bb11525ed33c104b2a275876d1d0992
A-Danylevych <anton231255@gmail.com>
2021-12-08 17:36:28 +0200

    name fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float steerSpeed = 1f;
     [SerializeField] private float moveSpeed = 1f;
